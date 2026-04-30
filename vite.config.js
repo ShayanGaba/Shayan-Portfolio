27f1770890d2006b0ed49a397e56558bf1a59933
Shayan Gaba <451shayan@gmail.com>
2026-05-01 00:06:14 +0500

    solved an issue

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,33 @@
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
 // });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import path from "path";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    dedupe: [
+      "react",
+      "react-dom",
+      "@react-three/fiber",
+      "@react-three/drei",
+      "three",
+    ],
+    alias: {
+      // Force single React instance
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    },
+  },
   build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // Important: don't let CJS modules create duplicate React
+      requireReturnsDefault: "preferred",
+    },
     assetsInlineLimit: 4096,
     rollupOptions: {
       output: {
